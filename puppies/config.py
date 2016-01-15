776e1fd0f16ba9289538d1e287d85416db6fcc34
Chris Mitchell <chris@donuts.io>
2016-01-15 15:51:26 +1100

    changed port number

--- puppies/config.py
+++ puppies/config.py
@@ -33,7 +33,7 @@ class TestConfig(BaseConfig):
 
 class ProdConfig(BaseConfig):
     LOGGING_LEVEL = logging.WARNING
-    SERVER_PORT = 20000
+    SERVER_PORT = 8080
     SERVER_IP = os.getenv("OPENSHIFT_PYTHON_IP")
     CLIENT_SECRETS_DIR = os.getenv('OPENSHIFT_DATA_DIR')
 
