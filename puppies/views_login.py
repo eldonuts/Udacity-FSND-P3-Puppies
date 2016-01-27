9c019faf04260082c9778137099fedc404a80f20
Chris Mitchell <chris@donuts.io>
2016-01-27 15:28:09 +1100

    formatting

--- puppies/views_login.py
+++ puppies/views_login.py
@@ -203,7 +203,7 @@ def logout():
             facebook_id = login_session['facebook_id']
             # The access token must me included to successfully logout
             access_token = login_session['access_token']
-            url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
+            url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id, access_token)
             h = httplib2.Http()
             result = h.request(url, 'DELETE')[1]
             print result
